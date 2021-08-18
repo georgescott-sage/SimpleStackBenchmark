f0a4c844d3d93baab64fe0bbae7562eb30b65131
george.scott <george.scott@sage.com>
2021-08-18 11:34:09 +0100

    add bench

--- Implemenations/StringIsNumber/StringIsNumberBenchmarks.cs
+++ Implemenations/StringIsNumber/StringIsNumberBenchmarks.cs
@@ -12,6 +12,13 @@ public class StringIsNumberBenchmarks
     [Benchmark]
     public void HashSetStringIsNumber()
     {
+        
+        StringIsNumber.IsNumberHashSet("1234567890j");
+    }
+
+    [Benchmark]
+    public void ListStringIsNumber()
+    {
         StringIsNumber.IsNumberHashSet("1234567890j");
     }
 }
