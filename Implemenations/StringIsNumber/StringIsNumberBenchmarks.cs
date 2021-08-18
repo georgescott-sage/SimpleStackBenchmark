b51477c375dc39f0a17b668537b55a1cbb620eea
george.scott <george.scott@sage.com>
2021-08-18 13:19:32 +0100

    use random number gen

--- Implemenations/StringIsNumber/StringIsNumberBenchmarks.cs
+++ Implemenations/StringIsNumber/StringIsNumberBenchmarks.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using BenchmarkDotNet.Attributes;
@@ -9,16 +10,26 @@ using SimpleStackBenchmark.Implemenations.StringIsNumber;
 [RankColumn]
 public class StringIsNumberBenchmarks
 {
+    private Random random = new Random();
+
     [Benchmark]
     public void HashSetStringIsNumber()
     {
-        
-        StringIsNumber.IsNumberHashSet("1234567890j");
+        StringIsNumber.IsNumberHashSet(GetNumber(100));
     }
 
     [Benchmark]
     public void ListStringIsNumber()
     {
-        StringIsNumber.IsNumberHashSet("1234567890j");
+        StringIsNumber.IsNumberList(GetNumber(100));
+    }
+
+    private string GetNumber(int digits)
+    {
+        if (digits <= 1) return "";
+
+        var _min = (int)Math.Pow(10, digits - 1);
+        var _max = (int)Math.Pow(10, digits) - 1;
+        return random.Next(_min, _max).ToString();
     }
 }
