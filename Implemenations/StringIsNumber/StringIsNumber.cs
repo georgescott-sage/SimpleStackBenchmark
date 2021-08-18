badf9eb6ea7fc766d5013cc94820aea3c26535aa
george.scott <george.scott@sage.com>
2021-08-18 11:33:42 +0100

    add list impl.

--- Implemenations/StringIsNumber/StringIsNumber.cs
+++ Implemenations/StringIsNumber/StringIsNumber.cs
@@ -5,11 +5,25 @@ namespace SimpleStackBenchmark.Implemenations.StringIsNumber
     public static class StringIsNumber
     {
         private readonly static HashSet<char> numbers = new HashSet<char> {'0','1','2','3','4','5','6','7','8','9'};
+        private readonly static List<char> numbersList = new List<char> {'0','1','2','3','4','5','6','7','8','9'};
+        
         public static bool IsNumberHashSet(string text)
         {
             foreach(char character in text.ToCharArray())
             {
                 if(!numbers.Contains(character))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsNumberList(string text)
+        {
+            foreach(char character in text.ToCharArray())
+            {
+                if(!numbersList.Contains(character))
                 {
                     return false;
                 }
